80ab2d6c3c1d2d7a16596a0715ad988c22d93510
Anton Guryanov <guryanov91@gmail.com>
2014-07-10 16:49:21 +0400

    Code to generate plot1

--- plot1.R
+++ plot1.R
@@ -1,16 +1,24 @@
-tmpfile <- tempfile()
-download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
-              destfile=tmpfile,
-              method="wget")
-data <- read.csv(unz(tmpfile, "household_power_consumption.txt"),
-                 header=T,
-                 sep=";",
-                 quote="",
-                 na.strings="?",
-                 colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-unlink(tmpfile)
+#tmpfile <- tempfile()
+#download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+#              destfile=tmpfile,
+#              method="wget")
+#data <- read.csv(unz(tmpfile, "household_power_consumption.txt"),
+#                 header=T,
+#                 sep=";",
+#                 quote="",
+#                 na.strings="?",
+#                 colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+#unlink(tmpfile)
 
-hist(data$Global_active_power, 
+data <- read.table("data/household_power_consumpiton.txt",
+                   header=TRUE,
+                   sep=";",
+                   quote="",
+                   na.strings="?",
+                   colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+data_sample <- data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+hist(data_sample$Global_active_power, 
      col="red", 
      main="Global Active Power",
      xlab="Global Active Power (kilowatts)",
